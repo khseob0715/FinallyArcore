2d9c09b31e24ad317960b2b8f0f51919eb261501
khseob0715 <khseob0715@naver.com>
2018-11-07 14:08:02 +0900

    add Scene and text typer

---
+++ Assets/MyScript/StoryScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StoryScript : MonoBehaviour {
+
+    public GameObject TextGroup;
+    private int cur_text_index = 0;
+
+    private int TextGroup_Size;
+    // Use this for initialization
+    void Start()
+    {
+        TextGroup_Size = TextGroup.gameObject.transform.childCount - 1;
+        //print(TextGroup_Size);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void nextStory()
+    {
+        print(cur_text_index);
+        if (cur_text_index == TextGroup_Size)
+        {
+            SceneManager.LoadScene("HelloAR");
+        }
+
+        TextGroup.gameObject.transform.GetChild(cur_text_index).gameObject.SetActive(false);
+        TextGroup.gameObject.transform.GetChild(++cur_text_index).gameObject.SetActive(true);
+
+       
+    }
+}
---
+++ Assets/MyScript/TextTypeWriter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextTypeWriter : MonoBehaviour {
+
+    private Text txt;
+    private string story;
+	// Use this for initialization
+	void Start () {
+        txt = this.GetComponent<Text>();
+        story = txt.text;
+        txt.text = "";
+
+        StartCoroutine("PlayText");
+    }
+
+
+    IEnumerator PlayText()
+    {
+        foreach (char c in story)
+        {
+            txt.text += c;
+            yield return new WaitForSeconds(0.08f);
+        }
+    }
+}
